95712af972a9b62fae8d075260848edf799e39fd
vigneshrajkumar <vigneshrajkumar@hotmail.com>
2020-07-07 21:50:45 +0530

    dump updates

---
+++ dump/_products.js
@@ -0,0 +1,289 @@
+module.exports = [{
+    _id: "100",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "101",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "102",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "103",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "104",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "105",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "106",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-1",
+}, {
+    _id: "107",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-2",
+}, {
+    _id: "108",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-2",
+}, {
+    _id: "109",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-2",
+}, {
+    _id: "110",
+    categories: ["0","1"],
+    name: "product a",
+    brand: "brand a",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-2",
+}, {
+    _id: "111",
+    categories: ["0","2"],
+    name: "product type b",
+    brand: "brand b",
+    product_image: "https://via.placeholder.com/200",
+    price: 356,
+    description: "generic product description",
+    quantity: 500,
+    reviews: [{
+        _id: "1000",
+        review_by: "-5000",
+        content: "generic review",
+        rating: 3,
+        createdAt: "bloop"
+    },
+    {
+        _id: "1001",
+        review_by: "-5001",
+        content: "generic review",
+        rating: 2,
+        createdAt: "bloop"
+    }],
+    storeID: "-2",
+}]
---
+++ dump/_stores.js
@@ -0,0 +1,7 @@
+module.exports = [{
+    _id: "-1",
+    name: "WS Retail India Pvt. Ltd",
+},{
+    _id: "-2",
+    name: "ICPA Health Products Ltd",
+} ]
